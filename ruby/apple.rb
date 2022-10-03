ce7e8c51d3d3ada42fdaa3d2bd7cf1775238c17e
EC2 Default User <ec2-user@ip-172-31-1-67.ap-northeast-1.compute.internal>
2022-10-03 05:25:51 +0000

    [Finish] 2-5

---
+++ ruby/apple.rb
@@ -0,0 +1,29 @@
+apple = "Yamanashi"
+
+if apple == "Aomori"
+  puts "このリンゴは青森県産です。"
+elsif apple == "Nagano"
+  puts "このリンゴは青森県産ではなく、長野県産です。"
+else
+  puts "このリンゴは青森県産でも長野県産でもありません。"
+end
+
+apple = "Aomori"
+
+if apple == "Aomori"
+  puts "このリンゴは青森県産です。"
+elsif apple == "Nagano"
+  puts "このリンゴは青森県産ではなく、長野県産です。"
+else
+  puts "このリンゴは青森県産でも長野県産でもありません。"
+end
+
+apple = "Nagano"
+
+if apple == "Aomori"
+  puts "このリンゴは青森県産です。"
+elsif apple == "Nagano"
+  puts "このリンゴは青森県産ではなく、長野県産です。"
+else
+  puts "このリンゴは青森県産でも長野県産でもありません。"
+end
